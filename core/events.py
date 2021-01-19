9b07faa19c335981c1d6bc8b169010cbfcf25926
Maxim <by_ke@mail.ru>
2021-01-19 20:39:15 +0300

    Added connect to database

--- core/events.py
+++ core/events.py
@@ -1,6 +1,10 @@
 async def on_startup(dispatcher):
-    pass
+    from core.db_wrapper import database
+    if not database.is_connected:
+        await database.connect()
 
 
 async def on_shutdown(dispatcher):
-    pass
+    from core.db_wrapper import database
+    if database.is_connected:
+        await database.disconnect()
